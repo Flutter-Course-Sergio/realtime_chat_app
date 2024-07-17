16d393fa3fd89d98d1ee2126605d45bfc1407e57
Sergio Barreras <sergiobg.isc@gmail.com>
2024-07-16 21:43:57 -0700

    ChatPage - Inicio

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -5,10 +5,43 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const avatarTextStyle = TextStyle(fontSize: 12);
+    const usernameTextStyle = TextStyle(color: Colors.black54, fontSize: 16);
+
     return Scaffold(
-      body: Center(
-        child: Text('ChatPage'),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 1,
+          title: Row(
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.blue[100],
+                child: Text('TE', style: avatarTextStyle),
+              ),
+              const SizedBox(width: 10),
+              Text(
+                'Abigail Amador',
+                style: usernameTextStyle,
+              )
+            ],
+          ),
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Flexible(
+                  child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (_, i) => Text(i.toString()),
+                reverse: true,
+              )),
+              const Divider(),
+              Container(
+                color: Colors.white,
+                height: 100,
+              )
+            ],
+          ),
+        ));
   }
 }
