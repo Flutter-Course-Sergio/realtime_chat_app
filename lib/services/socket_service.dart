630ab34af22ffdcf8802eeafca8109758479318b
Sergio Barreras <sergiobg.isc@gmail.com>
2024-07-20 13:47:02 -0700

    Autenticando el cliente conectado por sockets

--- lib/services/socket_service.dart
+++ lib/services/socket_service.dart
@@ -2,17 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:realtime_chat_app/global/environment.dart';
 import 'package:socket_io_client/socket_io_client.dart' as io;
 
+import 'auth_service.dart';
+
 enum ServerStatus { online, offline, connecting }
 
 class SocketService with ChangeNotifier {
   ServerStatus _serverStatus = ServerStatus.connecting;
   late io.Socket _socket;
 
-  void connect() {
+  void connect() async {
+    final token = await AuthService.getToken();
+
     _socket = io.io("https://${Environment.apiUrl}/", {
       'transports': ['websocket'],
       'autoConnect': true,
-      'forceNew': true
+      'forceNew': true,
+      'extraHeaders': {
+        'Authorization': 'Bearer $token'
+      }
     });
 
     _socket.onConnect((_) {
